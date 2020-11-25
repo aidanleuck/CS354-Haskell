5bb29084b2b4e21858db7cf6416af426c22111b3
malwaredllc <danielvegamyhre@gmail.com>
2020-11-24 17:32:36 -0700

    output space count

--- semantic-viz/app/Main.hs
+++ semantic-viz/app/Main.hs
@@ -5,27 +5,8 @@ import Text.Regex.PCRE.Light
 --import Data.ByteString.Char8 as C
 import Data.Maybe
 
-
--- function for counting spaces on a line, which we use to determine node placement in graph
--- countNumSpaces str = length ([x | x <- str, x == ' '])
--- countNumSpaces str = length (filter (\x -> x == ' ') str)
 isSpace s = s == ' '
 countNumSpaces str = length (filter isSpace str)
-
-data Graph a = Graph [(a,[a])] -- Graph is a list of origins paired with edgeends
-
-createGraph ::Eq a => [(a,a)] -> Graph a
-createGraph = undefined
-
-empty :: Graph a
-empty = Graph []
-
-insertVertex :: Eq a => a -> Graph a -> Graph a
-insertVertex = undefined -- insert if not already in the Graph (with empty edges)
-
-insertEdge :: Eq a => (a,a) -> Graph a -> Graph a
-insertEdge = undefined -- insert edge in list of origin
---do not forget to add origin, end if they don't exist
 
 split :: [Char] -> [String]
 split "" = [""]
@@ -38,6 +19,10 @@ parseFile :: String -> IO String
 parseFile fileName = do
     let inputLines = drop 4 . lines <$> readFile fileName
     outLines <- inputLines
+    let splitLines = map split outLines
+    let spaces = map head splitLines
+    let num_spaces = map length spaces
+    print num_spaces
     return (unlines outLines)
 
 main = do
@@ -49,16 +34,3 @@ main = do
     let x = parseFile "app/wn_output.txt"
     y <- x
     putStrLn y
-
-
-
---    case contents of
---        Just contents -> parse contents
---        Nothing -> return ()
-
---parse :: [Char] -> IO ()
---parse contents = group1 where
---    r = compile (C.pack "Sense 1(.*?)Sense") [dotall]
---    groups = match r (C.pack contents) []
---    group1 = Prelude.head groups
---    System.IO.putStrLn group1
