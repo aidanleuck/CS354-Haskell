fe69d63768473f477ea082bfb4f888cbe694d871
malwaredllc <danielvegamyhre@gmail.com>
2020-11-26 10:18:07 -0700

    thanksgiving work

--- semantic-viz/app/Main.hs
+++ semantic-viz/app/Main.hs
@@ -2,36 +2,49 @@ import Lib
 import System.IO
 import System.Process
 import Data.Maybe
+import Data.Map (Map)
 import qualified Data.Map as M
 
---buildAdjacencyList :: [Int] -> [String] -> M
-buildAdjacencyList keys vals hashmap = do
-    let map1 = M.insert 5 "Four" hashmap
-        map2 = M.insert 5 "Five" map1
-    return map2
+-- Builds an adjacency list using a list of words and their corresponding number of leading spaces
+--buildAdjacencyList :: [String] -> [Int] -> (Map String Int) -> IO ()
+--buildAdjacencyList [] [] hashmap = hashmap
+buildAdjacencyList (k:keys) (v:vals) hashmap = do
+    let map1 = M.insert k v hashmap
+    print $ M.toList map1
+    return map1
+        --map2 = buildAdjacencyList keys vals map1
+    -- return map1
 
 
+-- Counts spaces
 isSpace s = s == ' '
 countNumSpaces str = length (filter isSpace str)
 
+
+-- Splits a line on the '=' character. Used by the getSpaces function.
 split :: [Char] -> [String]
 split "" = [""]
 split ('=':cs) = "" : split cs
 split (c:cs) = (c:cellCompletion) : otherCells
  where cellCompletion : otherCells = split cs
 
+
+-- Gets lines from input file containing wordnet output, and removes headers from top
 getLines :: String -> IO [String]
 getLines fileName = do
-    let inputLines = drop 4 . lines <$> readFile fileName
+    let inputLines = drop 7 . lines <$> readFile fileName
     outLines <- inputLines
     return outLines
 
+
+-- Gets number of leading spaces from each line (to determine node placement in graph)
 getSpaces :: [String] -> [Int]
 getSpaces inputLines = do
     let splitLines = map split inputLines
-    let spaces = map head splitLines
-    let num_spaces = map length spaces
-    return (head num_spaces)
+        spaces = map head splitLines
+        num_spaces = map length spaces
+    return num_spaces
+
 
 main = do
     n <- getLine
@@ -43,7 +56,9 @@ main = do
     let inputLines = getLines "app/wn_output.txt"
     nonIOLines <- inputLines
     let inputSpaces = getSpaces nonIOLines
-        emptyHashMap = M.empty
-        adjacencyList = buildAdjacencyList inputSpaces inputLines
-    -- mapM_ (print . snd) (M.toList adjacencyList)
-    print $ (M.toList adjacencyList)
+        pairs = zip nonIOLines inputSpaces
+        hashmap = M.fromList pairs
+--        adjacencyList =  M.insert n 0 hashmap -- insert root node
+--        adjacencyList = buildAdjacencyList pairs hashmap
+--    print $ M.toList hashmap
+    print pairs
