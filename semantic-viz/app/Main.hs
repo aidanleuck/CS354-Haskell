bf5347d5e3cb5317dad7daa5bfff945c201f5771
malwaredllc <danielvegamyhre@gmail.com>
2020-11-26 17:25:05 -0700

    built out helper functions for building adjacency list

--- semantic-viz/app/Main.hs
+++ semantic-viz/app/Main.hs
@@ -5,30 +5,43 @@ import Data.Maybe
 import Data.Map (Map)
 import qualified Data.Map as M
 
--- Builds an adjacency list from a hashmap of key value pairs (word, num_spaces)
---buildAdjacencyList :: [String] -> [Int] -> (Map String Int) -> IO ()
---buildAdjacencyList [] [] hashmap = hashmap
-buildAdjacencyList [] prevParent currParent currNumSpaces hashmap = hashmap
-buildAdjacencyList pairs prevParent currParent currNumSpaces hashmap = do
+
+-- add neighbor to current node without updating current parent node pointer
+addNeighborSameParent currParent currNumSpaces word spaces hashmap = do
+    let vals = M.lookup currParent hashmap
+        newVals = (word:vals)
+    M.update currParent newVals hashmap
+    buildAdjacencyList pairs prevParent currParent currNumSpaces hashmap
+
+
+-- update current node to new word and add neighbor
+addNeighborNewParent currParent currNumSpaces word spaces hashmap = do
+    let currNumSpaces = spaces
+        prevParent = currParent
+        currParent = word
+    M.insert word [] hashmap
+    buildAdjacencyList pairs prevParent currParent currNumSpaces hashmap
+
+
+-- update current node to old parent and add neighbor
+addNeighborOldParent currParent currNumSpaces word spaces hashmap = do
+    let currNumSpaces = spaces
+        currParent = prevParent
+        vals = M.lookup currParent hashmap
+        newVals = (word:vals)
+    M.update currParent newVals hashmap
+    buildAdjacencyList pairs prevParent currParent currNumSpaces hashmap
+
+-- main function for building the adjacency list
+buildAdjacencyList [] parents currNumSpaces hashmap = hashmap
+buildAdjacencyList pairs parents currNumSpaces hashmap = do
     let next = head pairs
         word = head next
-        spaces = tail next
-    if spaces == currNumSpaces then
-        let vals = M.lookup currParent hashmap
-            vals = (word:vals)
-        M.update currParent vals hashmap
-    else if spaces > currNumSpaces then
-        let currNumSpaces = spaces
-            prevParent = currParent
-            currParent = word
-        M.insert word [] hashmap
-    else if spaces < currNumSpaces then
-        let currNumSpaces = spaces
-            currParent = prevParent
-            vals = M.lookup currParent hashmap
-            vals = (word:vals)
-        M.update currParent vals hashmap
-    buildAdjacencyList pairs prevParent currParent currNumSpaces hashmap
+        spaces = last next
+        currParent = last parents
+    if spaces > currNumSpaces then addNeighborNewParent currParent currNumSpaces word spaces hashmap
+    else if spaces < currNumSpaces then addNeighborOldParent currParent currNumSpaces word spaces hashmap
+    else addNeighborSameParent currParent currNumSpaces word spaces hashmap
 
 
 -- Counts spaces
@@ -58,9 +71,10 @@ getPairs inputLines = do
     let splitLines = map splitOnEqualSign inputLines  -- split input lines on '=' character
         spaces = map head splitLines -- get leading spaces on each line
         numSpaces = map length spaces -- measure number of leading spaces on each line
-        wordStrings = map head (map tail (map words inputLines)) -- get words on each line
+        wordStrings = map last (map words inputLines) -- get words on each line
         pairs = zip wordStrings numSpaces
     return pairs
+
 
 main = do
     category <- getLine
@@ -74,7 +88,9 @@ main = do
     let pairs = getPairs nonIOLines
         pairs = ((category, 0): pairs)
         emptyMap = M.empty
+        hashmap = M.insert category 0 emptyMap
 --        hashmapFromPairs = M.fromList (head pairs) -- construct hashmap of (word, num_spaces) pairs
 --        hashmapFromPairs =  M.insert n 0 hashmapFromPairs -- insert root node
-        adjacencyList = buildAdjacencyList pairs category category 0 emptyMap
-    print $ M.toList hashmap
+        adjacencyList = buildAdjacencyList pairs [category] 0 hashmap
+--    print $ M.toList hashmap
+    print pairs
